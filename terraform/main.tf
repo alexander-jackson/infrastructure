160118b5ad127abd5e667a3370a2b042ba73e223
Alexander Jackson <alexanderjackson@protonmail.com>
2022-05-01 11:22:36 +0100

    feat: add initial Terraform (#11)
    Add initial Terraform from the defined DigitalOcean state.

---
+++ terraform/main.tf
@@ -0,0 +1,88 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+variable "do_token" {
+  type = string
+}
+
+resource "digitalocean_project" "blackboards" {
+  name        = "blackboards"
+  purpose     = "Service or API"
+  environment = "Production"
+}
+
+resource "digitalocean_droplet" "main" {
+  name       = "main"
+  image      = "63663980"
+  region     = "lon1"
+  size       = "s-2vcpu-4gb"
+  monitoring = true
+}
+
+resource "digitalocean_domain" "blackboards" {
+  name = "blackboards.pl"
+}
+
+resource "digitalocean_record" "ns1" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns1.digitalocean.com."
+}
+
+resource "digitalocean_record" "ns2" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns2.digitalocean.com."
+}
+
+resource "digitalocean_record" "ns3" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "NS"
+  name   = "@"
+  value  = "ns3.digitalocean.com."
+}
+
+resource "digitalocean_record" "root" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_droplet.main.ipv4_address
+}
+
+resource "digitalocean_record" "opentracker" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "tracker"
+  value  = digitalocean_droplet.main.ipv4_address
+}
+
+resource "digitalocean_record" "webhooks" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "webhooks"
+  value  = digitalocean_droplet.main.ipv4_address
+}
+
+resource "digitalocean_record" "abs" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "abs"
+  value  = digitalocean_droplet.main.ipv4_address
+}
+
+resource "digitalocean_record" "sepl" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "sepl"
+  value  = digitalocean_droplet.main.ipv4_address
+}
+
+resource "digitalocean_record" "starling-webhooks" {
+  domain = digitalocean_domain.blackboards.id
+  type   = "A"
+  name   = "starling-webhooks"
+  value  = digitalocean_droplet.main.ipv4_address
+}
