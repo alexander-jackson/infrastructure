76764d7761f2f03a06e92a4160b095b0377460ea
Alexander Jackson <alexanderjackson@protonmail.com>
2023-05-07 13:59:47 +0100

    feat: add user with fewer permissions (#60)
    Logging in with the root account or performing operations with it is a
    bad idea, so we should have a separate user with reduced permissions.
    
    Note: these have all been applied locally during testing, so there
    should be no changes in the plan.
    
    This change:
    * Adds a user called `alex.jackson` with some basic permissions

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -16,6 +16,13 @@ resource "aws_iam_role" "iac_deployer" {
           Service = "s3.amazonaws.com"
         }
       },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_iam_user.personal.arn
+        }
+      },
     ]
   })
 }
@@ -39,4 +46,39 @@ resource "aws_iam_policy" "iac_deployer" {
 resource "aws_iam_role_policy_attachment" "iac_deployer" {
   role       = aws_iam_role.iac_deployer.name
   policy_arn = aws_iam_policy.iac_deployer.arn
+}
+
+resource "aws_iam_user" "personal" {
+  name = "alex.jackson"
+}
+
+resource "aws_iam_access_key" "personal" {
+  user    = aws_iam_user.personal.name
+  pgp_key = file("keys/pgp-b64.key")
+}
+
+resource "aws_iam_user_policy" "personal" {
+  name = format("%s.policy", aws_iam_user.personal.name)
+  user = aws_iam_user.personal.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:ListAllMyBuckets", "s3:ListBucket", "s3:Get*", "iam:ChangePassword"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = "sts:AssumeRole",
+        Effect   = "Allow",
+        Resource = aws_iam_role.iac_deployer.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_login_profile" "personal" {
+  user    = aws_iam_user.personal.name
+  pgp_key = file("keys/pgp-b64.key")
 }
