798a5b230a8a16312d1de03621f2b1ee51e48526
Alexander Jackson <alexanderjackson@protonmail.com>
2023-08-28 11:23:48 +0100

    feat: destroy most Digital Ocean infrastructure
    A lot of this is now handled on the AWS side, so we don't need it over
    here anymore. Route53 contains the DNS records and there's an EC2
    instance that serves `opentracker` traffic running `f2`.
    
    This change:
    * Removes the domain names
    * Removes the snapshot which has been sitting around
    * Removes the secondary instance that was running `opentracker`

--- terraform/digital_ocean.tf
+++ terraform/digital_ocean.tf
@@ -5,9 +5,6 @@ resource "digitalocean_project" "blackboards" {
   is_default  = true
 
   resources = [
-    digitalocean_domain.blackboards.urn,
-    digitalocean_domain.opentracker.urn,
-    digitalocean_droplet.secondary.urn,
     digitalocean_droplet.postgres.urn,
   ]
 }
@@ -17,28 +14,9 @@ resource "digitalocean_ssh_key" "m2" {
   public_key = file("keys/id_rsa.pub")
 }
 
-resource "digitalocean_ssh_key" "tara" {
-  name       = "tara"
-  public_key = file("keys/tara.pub")
-}
-
 resource "digitalocean_ssh_key" "secondary" {
   name       = "secondary"
   public_key = file("keys/secondary_id_rsa.pub")
-}
-
-resource "digitalocean_droplet_snapshot" "original-main-snapshot" {
-  droplet_id = 196151282
-  name       = "original-main-snapshot"
-}
-
-resource "digitalocean_droplet" "secondary" {
-  name       = "secondary"
-  image      = "ubuntu-22-10-x64"
-  region     = "lon1"
-  size       = "s-1vcpu-1gb"
-  monitoring = true
-  ssh_keys   = [23928565]
 }
 
 resource "digitalocean_droplet" "postgres" {
@@ -48,68 +26,4 @@ resource "digitalocean_droplet" "postgres" {
   size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys   = [digitalocean_ssh_key.m2.id, digitalocean_ssh_key.secondary.id]
-}
-
-resource "digitalocean_domain" "blackboards" {
-  name = "blackboards.pl"
-}
-
-resource "digitalocean_domain" "opentracker" {
-  name = "opentracker.app"
-}
-
-resource "digitalocean_record" "ns1" {
-  domain = digitalocean_domain.blackboards.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns1.digitalocean.com."
-}
-
-resource "digitalocean_record" "opentracker-ns1" {
-  domain = digitalocean_domain.opentracker.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns1.digitalocean.com."
-}
-
-resource "digitalocean_record" "ns2" {
-  domain = digitalocean_domain.blackboards.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns2.digitalocean.com."
-}
-
-resource "digitalocean_record" "opentracker-ns2" {
-  domain = digitalocean_domain.opentracker.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns2.digitalocean.com."
-}
-
-resource "digitalocean_record" "ns3" {
-  domain = digitalocean_domain.blackboards.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns3.digitalocean.com."
-}
-
-resource "digitalocean_record" "opentracker-ns3" {
-  domain = digitalocean_domain.opentracker.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns3.digitalocean.com."
-}
-
-resource "digitalocean_record" "opentracker-root" {
-  domain = digitalocean_domain.opentracker.id
-  type   = "A"
-  name   = "@"
-  value  = digitalocean_droplet.secondary.ipv4_address
-}
-
-resource "digitalocean_record" "opentracker-testing" {
-  domain = digitalocean_domain.opentracker.id
-  type   = "A"
-  name   = "testing"
-  value  = "63.32.120.39"
 }
