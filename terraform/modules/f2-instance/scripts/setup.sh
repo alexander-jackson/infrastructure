ffc7247c5d6d53027ad6fcb2304db958823a91eb
Alexander Jackson <alexanderjackson@protonmail.com>
2023-08-29 06:28:57 +0100

    feat: add module for `f2` (#86)
    Terraform is designed to be composable, but the current set of
    definitions is just a big ball of spaghetti. There's an `s3-bucket`
    module which is useful, but it would also be good to have a module for
    creating instances running `f2` for the purposes of upgrades.
    
    Additionally, there's some user setup required at the moment to install
    Docker, update all the packages and then run the `f2` container. This
    can all be automated instead.
    
    This change:
    * Adds a module to create IAM roles, security groups and an EC2 instance
      running a specific version of the `f2` container
    * Creates a new instance using the module, with the expectation of
      failing it over to be the primary

---
+++ terraform/modules/f2-instance/scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update existing list of packages and install some basic ones
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Set up the Docker registry
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package list and install Docker
+sudo apt update
+sudo apt install -y docker-ce
+
+# Allow the `ubuntu` user to run `docker` commands (for SSH access)
+sudo usermod -aG docker ubuntu
+
+sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 443:443 alexanderjackson/f2:${tag} -- --config s3://configuration-sfvz2s/f2/config.yaml
