70bd1b6164d9e17a7f8ca18481c8ec3bce824f51
Alexander Jackson <alexanderjackson@protonmail.com>
2023-10-18 17:40:06 +0100

    refactor: remove `random_id` configuration (#131)
    All buckets should now have a random identifier in the Terraform state,
    so there's no need to configure this anymore.
    
    This change:
    * Removes the usages
    * Inlines all the configuration

--- terraform/modules/s3-bucket/variables.tf
+++ terraform/modules/s3-bucket/variables.tf
@@ -3,12 +3,6 @@ variable "bucket_name" {
   description = "The name of the bucket to create"
 }
 
-variable "with_random_id" {
-  type        = bool
-  description = "Whether to use a `random_id` resource"
-  default     = false
-}
-
 variable "pending_deletion" {
   type        = bool
   description = "Whether the bucket is pending deletion and objects should be cleared out"
