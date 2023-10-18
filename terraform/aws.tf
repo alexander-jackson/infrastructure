1d1be264ce9019f964db62c97d5024e47f90893d
Alexander Jackson <alexanderjackson@protonmail.com>
2023-10-18 18:42:21 +0100

    feat: destroy legacy instance (#136)
    The traffic has been switched over to the new version, so we can destroy
    the old instance.
    
    This change:
    * Destroys the instance and everything under it

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -212,21 +212,6 @@ resource "aws_key_pair" "main" {
   public_key = file("./keys/id_rsa.pub")
 }
 
-module "primary" {
-  source = "./modules/f2-instance"
-
-  name          = "primary"
-  tag           = "20231007-1914"
-  config_arn    = module.config_bucket.arn
-  vpc_id        = aws_vpc.main.id
-  subnet_id     = aws_subnet.main.id
-  ami           = "ami-0ab14756db2442499"
-  instance_type = "t2.nano"
-  key_name      = aws_key_pair.main.key_name
-  config_bucket = module.config_bucket.name
-  config_key    = "f2/config.yaml"
-}
-
 module "secondary" {
   source = "./modules/f2-instance"
 
