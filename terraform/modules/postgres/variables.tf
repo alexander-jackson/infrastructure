552fe198cc9b4c6e7ae2711dfc251f1ef40b70ab
Alexander Jackson <alexanderjackson@protonmail.com>
2023-12-29 07:29:16 +0000

    refactor: Postgres instance parameters (#156)
    As was done for the `f2-instance` module, it's easier to read when the
    parameters are somewhat grouped by functionality.
    
    This change:
    * Does the same for the Postgres module

--- terraform/modules/postgres/variables.tf
+++ terraform/modules/postgres/variables.tf
@@ -3,49 +3,25 @@ variable "name" {
   description = "Unique name of the instance"
 }
 
-variable "major_version" {
-  type        = string
-  description = "The major version of Postgres to run on the instance"
+variable "instance" {
+  type = object({
+    type              = string
+    ami               = string
+    vpc_id            = string
+    subnet_id         = string
+    availability_zone = string
+  })
+  description = "Parameters for the underlying EC2 instance"
 }
 
-variable "backup_bucket" {
-  type        = string
-  description = "The name of the backup bucket"
-}
-
-variable "configuration_bucket" {
-  type        = string
-  description = "The name of the configuration bucket"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "The identifier of the VPC for the security groups"
-}
-
-variable "subnet_id" {
-  type        = string
-  description = "The identifier of the subnet to place the instance in"
-}
-
-variable "availability_zone" {
-  type        = string
-  description = "The availability zone for the instance and storage volume"
-}
-
-variable "storage_size" {
-  type        = number
-  description = "The amount of storage to allocate for the instance in GB"
-}
-
-variable "ami" {
-  type        = string
-  description = "The ami to use for the instance"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "The class/type to use for the instance"
+variable "configuration" {
+  type = object({
+    major_version        = string
+    storage_size         = number
+    backup_bucket        = string
+    configuration_bucket = string
+  })
+  description = "Parameters for the underlying Postgres install"
 }
 
 variable "key_name" {
