b10151d4012268dc5f4dbb36024dfa9c9d08f4dd
Alexander Jackson <alexanderjackson@protonmail.com>
2023-12-30 21:30:49 +0000

    fix: correct parameter name

--- terraform/modules/f2-instance/main.tf
+++ terraform/modules/f2-instance/main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group_rule" "allow_inbound_ipv6_ssh" {
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.this.id
-  cidr_blocks       = ["::/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 resource "aws_security_group_rule" "allow_inbound_https" {
