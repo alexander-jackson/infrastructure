3501b656e5328f6d054092f99ec202ef580e7c96
Alexander Jackson <alexanderjackson@protonmail.com>
2024-01-16 19:59:45 +0000

    deps: upgrade versions
    We're running some older versions of providers, so let's upgrade them.
    
    This change:
    * Bumps the `aws` and `digitalocean` providers to the latest versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "=2.34.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "=5.32.1"
     }
   }
 
