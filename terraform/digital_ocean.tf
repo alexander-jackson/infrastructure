1f39a2a62ec8b77d42f6a5005748d39226380fb8
Alexander Jackson <alexanderjackson@protonmail.com>
2024-03-09 18:14:12 +0000

    feat: remove Digital Ocean file
    There's nothing being hosted in Digital Ocean anymore so we can empty
    the file. This should delete the project (if required) and then we can
    remove all the references.

--- terraform/digital_ocean.tf
+++
@@ -1,8 +0,0 @@
-resource "digitalocean_project" "blackboards" {
-  name        = "blackboards"
-  purpose     = "Service or API"
-  environment = "Production"
-  is_default  = true
-
-  resources = []
-}
