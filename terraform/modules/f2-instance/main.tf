c63c887c54fbcda5625d2f73395f3da84ea42285
Alexander Jackson <alexanderjackson@protonmail.com>
2024-04-06 12:24:05 +0100

    feat: remove IPv6 setup
    The IPv6 setup isn't being used at the moment and will probably be
    revisited through an entire migration of the infrastructure. Let's
    remove it for now as it has been causing some problems.
    
    This change:
    * Removes it from the instances and subnet

--- terraform/modules/f2-instance/main.tf
+++ terraform/modules/f2-instance/main.tf
@@ -74,16 +74,6 @@ resource "aws_security_group_rule" "allow_inbound_ssh" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "allow_inbound_ipv6_ssh" {
-  description       = "Allow inbound SSH from anywhere on IPv6"
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = aws_security_group.this.id
-  ipv6_cidr_blocks  = ["::/0"]
-}
-
 resource "aws_security_group_rule" "allow_inbound_https" {
   description       = "Allow inbound HTTPS from anywhere"
   type              = "ingress"
@@ -92,16 +82,6 @@ resource "aws_security_group_rule" "allow_inbound_https" {
   protocol          = "tcp"
   security_group_id = aws_security_group.this.id
   cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "allow_inbound_ipv6_https" {
-  description       = "Allow inbound HTTPS from anywhere on IPv6"
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.this.id
-  ipv6_cidr_blocks  = ["::/0"]
 }
 
 resource "aws_security_group_rule" "allow_outbound_ssh" {
@@ -172,7 +152,6 @@ resource "aws_instance" "this" {
 
   vpc_security_group_ids = [aws_security_group.this.id]
   iam_instance_profile   = aws_iam_instance_profile.this.name
-  ipv6_address_count     = var.instance.ipv6_address_count
 
   metadata_options {
     # Since we'll be running containers, we need an extra hop
