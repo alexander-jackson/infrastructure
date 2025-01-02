07522f144943db155be862b2ddfc4c9a50326f2d
Alexander Jackson <alexanderjackson@protonmail.com>
2025-01-02 10:00:47 +0000

    feat: failover instance
    Planning to get some time to actually look at this during the weekend...

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -193,9 +193,9 @@ resource "aws_key_pair" "main" {
   public_key = file("./keys/id_rsa.pub")
 }
 
-module "secondary" {
+module "primary" {
   source = "./modules/f2-instance"
-  name   = "secondary"
+  name   = "primary"
 
   instance = {
     type      = "t2.nano"
@@ -250,13 +250,13 @@ module "database" {
   elastic_ip = false
 }
 
-resource "aws_security_group_rule" "allow_inbound_connections_from_secondary" {
-  description              = format("Allow inbound connections from %s", module.secondary.security_group_id)
+resource "aws_security_group_rule" "allow_inbound_connections_from_primary" {
+  description              = format("Allow inbound connections from %s", module.primary.security_group_id)
   type                     = "ingress"
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  source_security_group_id = module.secondary.security_group_id
+  source_security_group_id = module.primary.security_group_id
   security_group_id        = module.database.security_group_id
 }
 
@@ -297,5 +297,5 @@ resource "aws_route53_record" "records" {
   name    = each.key
   type    = "A"
   ttl     = 300
-  records = [module.secondary.public_ip]
+  records = [module.primary.public_ip]
 }
