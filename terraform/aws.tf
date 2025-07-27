26d86abdb76e8d08f7346979fc3542b941ee69ab
Alexander Jackson <alexanderjackson@protonmail.com>
2025-07-27 20:06:59 +0100

    feat: swap traffic (#296)
    The new instance is healthy and seems to be able to process traffic, so
    let's swap over to it.
    
    This change:
    * Updates the DNS records

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -556,7 +556,7 @@ resource "aws_route53_record" "records" {
   name    = each.key
   type    = "A"
   ttl     = 300
-  records = [module.primary.public_ip]
+  records = [module.secondary.public_ip]
 }
 
 resource "aws_route53_record" "telemetry_records" {
@@ -578,7 +578,7 @@ resource "aws_route53_record" "forkup_records" {
   name    = each.key
   type    = "A"
   ttl     = 300
-  records = [module.primary.public_ip]
+  records = [module.secondary.public_ip]
 }
 
 # Internal Route 53 definitions
